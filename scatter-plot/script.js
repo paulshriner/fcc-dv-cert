056917af849b499ea7df550094f7f30789691b9b
paulshriner <shrinerpaul@gmail.com>
2024-12-24 13:15:48 -0500

    scatter-plot: add legend

--- scatter-plot/script.js
+++ scatter-plot/script.js
@@ -45,8 +45,10 @@ fetch(url)
        .attr("id", "x-axis");
     svg.append("text")
        .text("Year")
-       .attr("x", 400)
-       .attr("y", 480);
+       .attr("x", 405)
+       .attr("y", 475)
+       .attr("id", "x-label")
+       .style("font-size", "10px");
 
     // y axis
     svg.append("g")
@@ -56,8 +58,10 @@ fetch(url)
     svg.append("text")
        .text("Time in Minutes")
        .attr("transform", "rotate(-90)")
-       .attr("x", -310)
-       .attr("y", 15);
+       .attr("x", -290)
+       .attr("y", 15)
+       .attr("id", "y-label")
+       .style("font-size", "10px");
 
     // plot points using year as x and time as y
     svg.selectAll("circle")
@@ -69,5 +73,39 @@ fetch(url)
        .attr("r", r)
        .attr("class", "dot")
        .attr("data-xvalue", d => d["Year"])
-       .attr("data-yvalue", d => parse(d["Time"]));
+       .attr("data-yvalue", d => parse(d["Time"]))
+       .style("fill", d => d["Doping"] === "" && "lightgreen");
+
+    // create legend container
+    const legend = svg.append("g")
+                      .attr("id", "legend");
+    
+    // create no doping text
+    legend.append("text")
+          .text("No doping allegations")
+          .attr("x", 621)
+          .attr("y", 300)
+          .style("font-size", "10px");
+   
+    // create no doping shape
+    legend.append("rect")
+          .attr("x", 730)
+          .attr("y", 287)
+          .attr("width", 15)
+          .attr("height", 15)
+          .style("fill", "lightgreen");
+
+    // create no doping text
+    legend.append("text")
+          .text("Riders with doping allegations")
+          .attr("x", 580)
+          .attr("y", 320)
+          .style("font-size", "10px");
+   
+    // create no doping shape
+    legend.append("rect")
+          .attr("x", 730)
+          .attr("y", 307)
+          .attr("width", 15)
+          .attr("height", 15);   
 });
