bd2556e877d81ee4a6e665f6fad0b594d6c89b18
paulshriner <shrinerpaul@gmail.com>
2024-12-27 13:59:09 -0500

    choropleth-map: add data-fips, data-education to counties

--- choropleth-map/script.js
+++ choropleth-map/script.js
@@ -15,14 +15,17 @@ const cSVG = graph.append("svg")
                  .attr("width", cw)
                  .attr("height", ch);
 
-// Draw the map
-fetch(url[1])
-.then(res => res.json())
-.then(data => {
+// Thanks https://stackoverflow.com/questions/59037553/fetch-multiple-urls-at-the-same-time for Promise.all
+// This allows fetching multiple urls at the same time
+Promise.all([
+    fetch(url[0]).then(res => res.json()),
+    fetch(url[1]).then(res => res.json())
+]).then(data => {
+    // This was helpful for starting tips: https://forum.freecodecamp.org/t/data-visualization-choropleth-map-tips-to-start/594672/2
     // Thanks https://gis.stackexchange.com/questions/310209/converting-topojson-data-to-geojson-using-javascript-in-browser for topojson.feature()
-    const nation = topojson.feature(data, data.objects.nation);
-    const states = topojson.feature(data, data.objects.states).features;
-    const counties = topojson.feature(data, data.objects.counties).features;
+    const nation = topojson.feature(data[1], data[1].objects.nation);
+    const states = topojson.feature(data[1], data[1].objects.states).features;
+    const counties = topojson.feature(data[1], data[1].objects.counties).features;
     
     // Thanks https://billmill.org/making_a_us_map.html for help with drawing the map
     // Draw the nation
@@ -48,5 +51,7 @@ fetch(url[1])
         .data(counties)
         .join("path")
         .attr("d", d3.geoPath())
-        .attr("class", "county");
-})
+        .attr("class", "county")
+        .attr("data-fips", d => d.id)
+        .attr("data-education", d => data[0].filter(i => i.fips === d.id)[0].bachelorsOrHigher);
+});
